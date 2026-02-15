896fed4964a6d663c3f78c18efa5cb6cb8166ac5
Thomas R. Dial <dialtr@gmail.com>
2026-02-15 15:51:36 -0500

    add tests for AsDouble()

--- token_tests.cc
+++ token_tests.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 
 #include "absl/status/status.h"
@@ -20,14 +21,40 @@ TEST(TokenTest, AsInteger_Success) {
   EXPECT_THAT(result.value(), 717);
 }
 
-TEST(TokenTest, AsInteger_Failure) {
+TEST(TokenTest, AsInteger_Failure_Invalid) {
   Token token(TokenType::kNumber, "abc");
   auto result = token.AsInteger();
-  std::cout << "result: " << result << std::endl;
   ASSERT_FALSE(result.ok()) << result.status();
-  //	EXPECT_THAT(result.status(), absl::StatusCode::kOutOfRange);
+  EXPECT_THAT(result.status().code(), absl::StatusCode::kInvalidArgument);
 }
 
-// TEST(LexerTest, AssertTrueExample) { EXPECT_TRUE(true); }
+TEST(TokenTest, AsInteger_Failure_Range) {
+  Token token(TokenType::kNumber, "9999999999999999999");
+  auto result = token.AsInteger();
+  ASSERT_FALSE(result.ok()) << result.status();
+  EXPECT_THAT(result.status().code(), absl::StatusCode::kOutOfRange);
+}
 
-// TEST(LexerTest, EqualityExample) { ASSERT_EQ(1 + 1, 2); }
+TEST(TokenTest, AsDouble_Success) {
+  Token token(TokenType::kNumber, "717.0");
+  auto result = token.AsInteger();
+  ASSERT_TRUE(result.ok()) << result.status();
+  const double expected = 717.0f;
+  const double diff = abs(expected - result.value());
+  const double kEpsilon = 0.00000001f;
+  EXPECT_TRUE(diff < kEpsilon);
+}
+
+TEST(TokenTest, AsDouble_Failure_Invalid) {
+  Token token(TokenType::kNumber, "abc");
+  auto result = token.AsInteger();
+  ASSERT_FALSE(result.ok()) << result.status();
+  EXPECT_THAT(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
+TEST(TokenTest, AsDoubler_Failure_Range) {
+  Token token(TokenType::kNumber, "1.8e309");
+  auto result = token.AsDouble();
+  ASSERT_FALSE(result.ok()) << result.status();
+  EXPECT_THAT(result.status().code(), absl::StatusCode::kOutOfRange);
+}
